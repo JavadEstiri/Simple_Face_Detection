8d0563ee2efc110d9ce0321abe102ddd912bb40e
Iman Estiri <is812818@gmail.com>
2022-11-04 13:42:25 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Simple_Face_Detection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.btn_Detect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic.Location = new System.Drawing.Point(7, 6);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(1204, 760);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // btn_Detect
+            // 
+            this.btn_Detect.Location = new System.Drawing.Point(1217, 396);
+            this.btn_Detect.Name = "btn_Detect";
+            this.btn_Detect.Size = new System.Drawing.Size(205, 59);
+            this.btn_Detect.TabIndex = 1;
+            this.btn_Detect.Text = "&Detect";
+            this.btn_Detect.UseVisualStyleBackColor = true;
+            this.btn_Detect.Click += new System.EventHandler(this.btn_Detect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1434, 771);
+            this.Controls.Add(this.btn_Detect);
+            this.Controls.Add(this.pic);
+            this.Name = "Form1";
+            this.Text = "Simple Face Detection";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btn_Detect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Simple_Face_Detection
+{
+    public partial class Form1 : Form
+    {
+
+        private readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Detect_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog fileDialog = new OpenFileDialog() { Filter = "JPEG|*.jpg" , Multiselect = false })
+            {
+                if(fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Image img = Image.FromFile(fileDialog.FileName);
+                    if (img == null) return;
+                    int orientationId = 0x0112;
+                    if (img.PropertyIdList.Contains(orientationId))
+                    {
+                        var orientation = (int)img.GetPropertyItem(orientationId).Value[0];
+                        var rotateFlip = RotateFlipType.RotateNoneFlipNone;
+                        switch (orientation)
+                        {
+                            case 1: rotateFlip = RotateFlipType.RotateNoneFlipNone; break;
+                            case 2: rotateFlip = RotateFlipType.RotateNoneFlipX; break;
+                            case 3: rotateFlip = RotateFlipType.Rotate180FlipNone; break;
+                            case 4: rotateFlip = RotateFlipType.Rotate180FlipX; break;
+                            case 5: rotateFlip = RotateFlipType.Rotate90FlipX; break;
+                            case 6: rotateFlip = RotateFlipType.Rotate90FlipNone; break;
+                            case 7: rotateFlip = RotateFlipType.Rotate270FlipX; break;
+                            case 8: rotateFlip = RotateFlipType.Rotate270FlipNone; break;
+                            default: rotateFlip = RotateFlipType.RotateNoneFlipNone; break;
+                        }
+                        if (rotateFlip != RotateFlipType.RotateNoneFlipNone)
+                        {
+                            img.RotateFlip(rotateFlip);
+                            img.RemovePropertyItem(orientationId);
+                        }
+                    }
+
+                    pic.Image = img;
+                    var bitmap = new Bitmap(pic.Image);
+                    Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+                    Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.4, 0);
+                    foreach(Rectangle rectangle in rectangles)
+                    {
+                        using(Graphics graphics = Graphics.FromImage(bitmap))
+                        {
+                            using (Pen pen = new Pen(Color.Red,1))
+                            {
+                                graphics.DrawRectangle(pen, rectangle);
+                            }
+                        }
+                    }
+                    pic.Image = bitmap;
+                }
+            }
+        }
+
+        private void CorrectExifOrientation(Image image)
+        {
+            if (image == null) return;
+            int orientationId = 0x0112;
+            if (image.PropertyIdList.Contains(orientationId))
+            {
+                var orientation = (int)image.GetPropertyItem(orientationId).Value[0];
+                var rotateFlip = RotateFlipType.RotateNoneFlipNone;
+                switch (orientation)
+                {
+                    case 1: rotateFlip = RotateFlipType.RotateNoneFlipNone; break;
+                    case 2: rotateFlip = RotateFlipType.RotateNoneFlipX; break;
+                    case 3: rotateFlip = RotateFlipType.Rotate180FlipNone; break;
+                    case 4: rotateFlip = RotateFlipType.Rotate180FlipX; break;
+                    case 5: rotateFlip = RotateFlipType.Rotate90FlipX; break;
+                    case 6: rotateFlip = RotateFlipType.Rotate90FlipNone; break;
+                    case 7: rotateFlip = RotateFlipType.Rotate270FlipX; break;
+                    case 8: rotateFlip = RotateFlipType.Rotate270FlipNone; break;
+                    default: rotateFlip = RotateFlipType.RotateNoneFlipNone; break;
+                }
+                if (rotateFlip != RotateFlipType.RotateNoneFlipNone)
+                {
+                    image.RotateFlip(rotateFlip);
+                    image.RemovePropertyItem(orientationId);
+                }
+            }
+        }
+    }
+}
